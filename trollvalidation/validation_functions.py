Fixed syntax error referring to non-existing variables, pointing to the proper path for the new dataset, and setting the computation machine back to trusty from xenial.

trollvalidation/validation_functions.py
@@ -93,28 +93,28 @@ def ice_std_dev_for_high_in_eval(data_eval, data_orig):
 
 
 def water_bias(data_eval, data_orig):
-    mask_expr = ~((data_eval >= 0) * (data_evel < 10))
+    mask_expr = ~((data_eval >= 0) * (data_eval < 10))
     only_water_in_chart = np.ma.array(data_eval, mask=mask_expr)
     diff = data_orig - only_water_in_chart
     return diff.mean()
 
 
 def water_std_dev(data_eval, data_orig):
-    mask_expr = ~((data_eval >= 0) * (data_evel < 10))
+    mask_expr = ~((data_eval >= 0) * (data_eval < 10))
     only_water_in_chart = np.ma.array(data_eval, mask=mask_expr)
     diff = data_orig - only_water_in_chart
     return diff.std()
 
 
 def intermediate_bias(data_eval, data_orig):
-    mask_expr = ~((data_eval >= 10) * (data_evel < 90))
+    mask_expr = ~((data_eval >= 10) * (data_eval < 90))
     intermediate_ice_in_chart = np.ma.array(data_eval, mask=mask_expr)
     diff = data_orig - intermediate_ice_in_chart
     return diff.mean()
 
 
 def intermediate_std_dev(data_eval, data_orig):
-    mask_expr = ~((data_eval >= 10) * (data_evel < 90))
+    mask_expr = ~((data_eval >= 10) * (data_eval < 90))
     intermediate_ice_in_chart = np.ma.array(data_eval, mask=mask_expr)
     diff = data_orig - intermediate_ice_in_chart
     return diff.std()