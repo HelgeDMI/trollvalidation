Fixed bug with masking after sigrid decoding and modified validation functions

trollvalidation/data_decoders/sigrid_decoder.py
@@ -109,18 +109,18 @@ class DecodeSIGRIDCodes(object):
 
         # Convert the sigrid codes to upper and lower limits of ice concentration
 
-        de = data_ref
-        cl = 10 * (de // 10)  # lower code: 1st digit * 10
-        cu = 10 * (de % 10)   # upper code: 2nd digit * 10
+        dr = data_ref
+        cl = 10 * (dr // 10)  # lower code: 1st digit * 10
+        cu = 10 * (dr % 10)   # upper code: 2nd digit * 10
         condition_choices = [
-            (de == 255, np.nan, np.nan),
-            (de == 00, 0,  0),
-            (de == 01, 0, 10),
-            (de == 02, 0, 10),  # TODO: Check this
-            (de == 92, 100, 100),
+            (dr == 255, np.nan, np.nan),
+            (dr == 00, 0,  0),
+            (dr == 01, 0, 10),
+            (dr == 02, 0, 10),  # TODO: Check this
+            (dr == 92, 100, 100),
             (cu == 10, cl, 100),        # 21, 31, 41, 51, 61, 71, 81, 91
-            (cu == 00, de, de),         # 10, 20, 30, 40, 50, 60, 70, 80, 90
-            (~(de % 10 == 0), cl, cu)   # 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25,
+            (cu == 00, dr, dr),         # 10, 20, 30, 40, 50, 60, 70, 80, 90
+            (~(dr % 10 == 0), cl, cu)   # 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25,
                                         # 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45,
                                         # 46, 47, 48, 49, 56, 57, 58, 59, 67, 68, 69,
                                         # 78, 79, 89
@@ -130,7 +130,7 @@ class DecodeSIGRIDCodes(object):
         upper_limits = np.select(condition, upper_limit, default=-128)
 
         if (-128 in lower_limits) or (-128 in upper_limits):
-            raise ValueError('Values {} not decoded'.format(de[(lower_limits == -128) | (upper_limits == -128)]))
+            raise ValueError('Values {} not decoded'.format(dr[(lower_limits == -128) | (upper_limits == -128)]))
 
         try:
             assert np.any((upper_limits >= lower_limits) | np.isnan(upper_limits) | np.isnan(lower_limits))
@@ -144,19 +144,21 @@ class DecodeSIGRIDCodes(object):
         #   * otherwise, reference is equal to the closest limit of the NIC interval.
         # If the NIC codes are specific values, rather that limits, 'reference' is equal to data_ref
 
-        do, ll, ul = data_test, lower_limits, upper_limits
+        dt, ll, ul = data_test, lower_limits, upper_limits
         # 'condition_choice' is a follows: [(interval, choice),...]
-        condition_choice = [((do >= ll) & (do <= ul), do),
-                            (do < ll,                 ll),
-                            (do > ul,                 ul)]
+
+        condition_choice = [((dt >= ll) & (dt <= ul), dt),
+                             (dt < ll,                ll),
+                             (dt > ul,                ul)]
         cond, choice = zip(*condition_choice)
-        reference = np.select(cond, choice, default=de)
+        reference = np.select(cond, choice, default=np.nan)
 
         if np.any(np.isnan(reference)):
             LOG.info('The file contains undetermined values')
 
+        reference = ma.masked_invalid(reference)
         if isinstance(data_ref, np.ma.core.MaskedArray):
-            reference = ma.masked_invalid(data_ref)
+            reference = ma.array(reference, mask=data_ref.mask)
 
         return reference, lower_limits, upper_limits
 

trollvalidation/data_preparation.py
@@ -101,12 +101,12 @@ def handle_binfile(bin_file, test_file, test_data):
 def handle_sigfile(sig_file, test_file, test_data):
 
     sig_reader = SIGFileReader()
-    ref_file_data = sig_reader.read_data(sig_file, test_file)
+    ref_file_data, lats, lons = sig_reader.read_data(sig_file, test_file)
 
     decoder = DecodeSIGRIDCodes()
     LOG.info('Decoding sig file with values: {}'.format(np.unique(ref_file_data, return_counts=False)))
     ref_data = decoder.sigrid_decoding(ref_file_data, test_data)
-    return ref_data, low_lim, upp_lim
+    return ref_data, low_lim, upp_lim, lats, lons
 
 
 # def handle_osi_ice_conc_nc_file(input_file):
@@ -159,7 +159,7 @@ def handle_osi_ice_conc_nc_file(input_file):
     status_flag = dataset.variables['status_flag'][0][:]
     ice_conc = np.ma.array(ice_conc, mask=(status_flag != 0))
     ice_conc = np.ma.masked_outside(ice_conc, -0.01, 100.01)
-    return ice_conc
+    return ice_conc, dataset.variables['lat'][0], dataset.variables['lon'][0]
 
 
 def handle_osi_ice_conc_nc_file_osi450(input_file, draft):
@@ -192,4 +192,4 @@ def handle_osi_ice_conc_nc_file_osi450(input_file, draft):
              status_flag & 2 == 2, status_flag & 8 == 8, status_flag & 1 == 1))
     ice_conc = np.ma.array(ice_conc, mask=mask_flags)
     ice_conc = np.ma.masked_outside(ice_conc, -0.01, 100.01)
-    return ice_conc
+    return ice_conc, dataset.variables['lat'][0], dataset.variables['lon'][0]

trollvalidation/validation_functions.py
@@ -25,27 +25,47 @@ def total_std_dev(data_ref, data_test):
 
 
 def ice_bias(data_ref, data_test):
-    only_ice_in_chart = np.ma.array(data_ref, mask=data_ref == 0)
+    only_ice_in_chart = np.ma.masked_outside(data_ref, 99, 100.1)
     diff = data_test - only_ice_in_chart
     return diff.mean()
 
 
 def ice_std_dev(data_ref, data_test):
-    only_ice_in_chart = np.ma.array(data_ref, mask=data_ref == 0)
+    only_ice_in_chart = np.ma.masked_outside(data_ref, 99, 100.1)
     diff = data_test - only_ice_in_chart
     return diff.std()
 
 
 def water_bias(data_ref, data_test):
-    only_water_in_chart = np.ma.array(data_ref, mask=data_ref > 0)
+    only_water_in_chart = np.ma.masked_outside(data_ref, -0.1, 1)
     diff = data_test - only_water_in_chart
     return diff.mean()
 
 
 def water_std_dev(data_ref, data_test):
-    only_water_in_chart = np.ma.array(data_ref, mask=data_ref > 0)
+    only_water_in_chart = np.ma.masked_outside(data_ref, -0.1, 1)
     diff = data_test - only_water_in_chart
     return diff.std()
+
+
+def intermediate_bias(data_ref, data_test):
+    only_intermediate_in_chart = np.ma.masked_outside(data_ref, 1, 99)
+    diff = data_test - only_intermediate_in_chart
+    return diff.mean()
+
+
+def intermediate_std_dev(data_ref, data_test):
+    only_water_in_chart = np.ma.masked_outside(data_ref, 1, 99)
+    diff = data_test - only_water_in_chart
+    return diff.std()
+
+
+# def intermediate_std_dev(data_ref, data_test):
+#     only_intermediate_in_chart = np.ma.masked_outside(data_ref, 1, 99)
+#     intermediate_ice_in_chart = np.ma.array(data_ref, mask=only_intermediate_in_chart)
+#     diff = data_test - intermediate_ice_in_chart
+#     return diff.std()
+
 
 
 def rmsdiff(data_ref, data_test):
@@ -120,16 +140,16 @@ def water_std_dev_for_low_in_eval(data_ref, data_test):
 #     diff = data_test - intermediate_ice_in_chart
 #     return diff.std()
 
-def intermediate_bias(data_ref, data_test):
-    mask_expr = ~((data_ref > 0) * (data_ref < 90))
-    intermediate_ice_in_chart = np.ma.array(data_ref, mask=mask_expr)
-    diff = data_test - intermediate_ice_in_chart
-    return diff.mean()
-
-
-def intermediate_std_dev(data_ref, data_test):
-    mask_expr = ~((data_ref > 0) * (data_ref < 90))
-    intermediate_ice_in_chart = np.ma.array(data_ref, mask=mask_expr)
-    diff = data_test - intermediate_ice_in_chart
-    return diff.std()
+# def intermediate_bias(data_ref, data_test):
+#     mask_expr = ~((data_ref > 0) * (data_ref < 90))
+#     intermediate_ice_in_chart = np.ma.array(data_ref, mask=mask_expr)
+#     diff = data_test - intermediate_ice_in_chart
+#     return diff.mean()
+#
+#
+# def intermediate_std_dev(data_ref, data_test):
+#     mask_expr = ~((data_ref > 0) * (data_ref < 90))
+#     intermediate_ice_in_chart = np.ma.array(data_ref, mask=mask_expr)
+#     diff = data_test - intermediate_ice_in_chart
+#     return diff.std()
 

validations/ice_conc/validation.py
@@ -74,11 +74,11 @@ def osi_ice_conc_pre_func(ref_time, ref_file, test_file):
     """
     temp_files = TmpFiles()
 
-    test_data = prepare_test_data(temp_files, test_file)
+    test_data, lat, lon = prepare_test_data(temp_files, test_file)
     ref_data, low_lim, upp_lim = prepare_ref_data(temp_files, ref_file, test_data, test_file)
 
     # Dump data to files for later visualization
-    dump_data(ref_time, ref_data, test_data, ref_file, test_file, low_lim, upp_lim)
+    dump_data(ref_time, ref_data, test_data, ref_file, test_file, low_lim, upp_lim, lat, lon)
 
     return PreReturn(temp_files, ref_data, test_data)
 
@@ -202,27 +202,25 @@ def collect_pickled_data(config):
 
     path_to_output = os.path.join(config.OUTPUT_DIR, config.PICKLED_DATA)
 
-    w = WriteNetCDF(path_to_output)
-
-    for ds_name, files in ds_source:
-        if files:
-            dates, data = stack_data(files)
-            w.np_array_to_nc(lat, 'lat', ('lat',),
-                             unit='degrees', longname='Latitude', least_significant_digit=1, fill_value=None)
-
-            w.np_array_to_nc(add_dim(np.array(dates)), 'time', ('time', 'lat', 'lon'))
-
-
-
-    # hdf5 = h5py.File(path_to_output, 'w')
-    # data_grp = hdf5.create_group('maps')
+    # w = WriteNetCDF(path_to_output)
+    #
     # for ds_name, files in ds_source:
     #     if files:
     #         dates, data = stack_data(files)
-    #         ds = data_grp.create_dataset(ds_name, data.shape, data=data.filled(),
-    #                                      dtype='f', compression="gzip")
-    #         ds.attrs['dates'] = np.array(dates)
-    # hdf5.close()
+    #         w.np_array_to_nc(lat, 'lat', ('lat',),
+    #                          unit='degrees', longname='Latitude', least_significant_digit=1, fill_value=None)
+    #
+    #         w.np_array_to_nc(add_dim(np.array(dates)), 'time', ('time', 'lat', 'lon'))
+
+    hdf5 = h5py.File(path_to_output, 'w')
+    data_grp = hdf5.create_group('maps')
+    for ds_name, files in ds_source:
+        if files:
+            dates, data = stack_data(files)
+            ds = data_grp.create_dataset(ds_name, data.shape, data=data.filled(),
+                                         dtype='f', compression="gzip")
+            ds.attrs['dates'] = np.array(dates)
+    hdf5.close()
 
 
 def cleanup(config):
@@ -250,5 +248,5 @@ def main(config):
     desc_str = config.SHORT_DESCRIPTION.format('SH', date.today())
     ice_conc_val_task(description=desc, description_str=desc_str)
 
-    if 'PICKLED_DATA' in config.__dict__.keys():
-        collect_pickled_data(config)
+    # if 'PICKLED_DATA' in config.__dict__.keys():
+    #     collect_pickled_data(config)